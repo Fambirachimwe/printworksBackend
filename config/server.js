35f27798cbe666af1603935a1959c8f1cebdc78e
modernlearningdev <81680713+modernlearningdev@users.noreply.github.com>
2021-06-30 06:29:06 +0200

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00d96dedcf64eb4d599451d9e32e999c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e50621c6-8e9a-42e7-bfb0-1aa8c25ab211'
+};
